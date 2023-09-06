f08ea6eebc47c8efc533ca4cb83705ecacb1c941
Alejandro Arriaga <19030837@itcelaya.edu.mx>
2023-09-05 23:52:07 -0600

    Aplicacion finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:concentric_transition/concentric_transition.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:practica1/card_planet.dart';
 
 void main() {
   runApp(const MainApp());
@@ -15,26 +17,64 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.green,
       ),
-      home: const Home(),
+      home: Home(),
     );
   }
 }
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  final data = [
+    CardPlanetData(
+      title: "Red Bull Racing",
+      subtitle: "Equipo de Fórmula 1",
+      image: const AssetImage("assets/images/team-1.png"),
+      backgroundColor:
+          const Color.fromARGB(255, 10, 3, 114), // Rojo para Red Bull
+      titleColor: Colors.white, // Texto en blanco
+      subtitleColor: Colors.yellow, // Texto en amarillo
+      background: LottieBuilder.asset("assets/animation/bg-1.json"),
+    ),
+    CardPlanetData(
+      title: "Ferrari",
+      subtitle: "Equipo de Fórmula 1",
+      image: const AssetImage("assets/images/team-2.png"),
+      backgroundColor: const Color.fromRGBO(255, 0, 0, 1), // Rojo para Ferrari
+      titleColor: Colors.black, // Texto en negro
+      subtitleColor: Colors.yellow, // Texto en amarillo
+      background: LottieBuilder.asset("assets/animation/bg-1.json"),
+    ),
+    CardPlanetData(
+      title: "Aston Martin",
+      subtitle: "Equipo de Fórmula 1",
+      image: const AssetImage("assets/images/team-3.png"),
+      backgroundColor:
+          const Color.fromARGB(255, 70, 133, 105), // Azul para Aston Martin
+      titleColor: Colors.white, // Texto en blanco
+      subtitleColor: Colors.green, // Texto en verde
+      background: LottieBuilder.asset("assets/animation/bg-1.json"),
+    ),
+    CardPlanetData(
+      title: "McLaren",
+      subtitle: "Equipo de Fórmula 1",
+      image: const AssetImage("assets/images/team-4.png"),
+      backgroundColor:
+          const Color.fromARGB(255, 255, 198, 145), // Rojo para McLaren
+      titleColor: Colors.white, // Texto en blanco
+      subtitleColor: Colors.orange, // Texto en naranja
+      background: LottieBuilder.asset("assets/animation/bg-1.json"),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ConcentricPageView(
-        colors: [Colors.white, Colors.blue, Colors.red],
-        physics: const NeverScrollableScrollPhysics(),
+        colors: data.map((e) => e.backgroundColor).toList(),
+        itemCount: data.length,
         itemBuilder: (int index) {
-          return Center(
-            child: Container(
-              child: Text('Page $index'),
-            ),
-          );
+          return CardPlanet(data: data[index]);
         },
       ),
     );
