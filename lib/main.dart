b5ebc80ac6ee9c1e66c7ae0242d280ba6528b9da
Alejandro Arriaga <19030837@itcelaya.edu.mx>
2023-09-05 22:48:55 -0600

    Vista básica de tarjetas

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:concentric_transition/concentric_transition.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,15 +6,36 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Onboarding',
+      theme: ThemeData(
+        primaryColor: Colors.green,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ConcentricPageView(
+        colors: [Colors.white, Colors.blue, Colors.red],
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (int index) {
+          return Center(
+            child: Container(
+              child: Text('Page $index'),
+            ),
+          );
+        },
       ),
     );
   }
